da5df658c5ec40cd6f8d2cee3c37ef31e81da6e0
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2021-04-03 22:26:50 -0500

    subiendo archivos
    A listando Archivos.

---
+++ js/forcestacks.js
@@ -0,0 +1,66 @@
+$('.step1-events').on('click', functionStep1(){        
+	const demoId = document.querySelector('step1');
+	demoId.display=	 'none';
+})
+
+$('.btn-show').on('click', function(){
+
+    $('form').animate({
+        top: '0px'
+    });
+
+    $('i').css({    	
+        display: 'none'
+    });
+    
+    $('.shape').fadeIn();      
+    
+})
+
+$('.shape').on('click', function(){
+    
+     $('form').animate({
+        top: '-545px'
+    });
+    
+    $('.shape').fadeOut();
+    
+    $('body').css({
+        overflowY: 'scroll'
+    })
+    
+})
+
+$('.load-image').on('click', function(){
+    
+    $('.seleted').click();
+    
+})
+
+
+/*VISOR DE FOTOS*/
+
+$(function() {
+  $('.seleted').change(function(e) {
+      addImage(e); 
+     });
+
+     function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+    
+      if (!file.type.match(imageType))
+       return;
+  
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+     }
+  
+     function fileOnload(e) {
+      var result=e.target.result;
+      $('.image-select').attr("src",result);
+     }
+});
+
+
