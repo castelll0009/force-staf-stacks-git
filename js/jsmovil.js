3b199006ac1d56185489500b121b8e6d19cf1219
castelll0009 <castelll0009@gmail.com>
2021-04-08 21:37:48 -0500

    Error con el despliegue de los subsubmenus TODO
    me estrese mucho y me puse a ver disney.

--- js/jsmovil.js
+++ js/jsmovil.js
@@ -1,10 +1,42 @@
-/*$('.submenu-ul ').hide();*/
+$('.submenu-ul ').hide();
 
 if($(window).width() > 1000){
-    $('.submenu-ul').bind
+    $('.submenu-ul ,li').bind('mousemove', function(){
+        $(this).children("ul").fadeIn(200);
+    })
+    $('.submenu-ul ,li').bind('mouseleave', function(){
+        $(this).children("ul").fadeOut(200);
+    })
+}else{
+    var show_submenu1 = 0;
+    var show_submenu2 = 0;
+
+    $('.submenu-ul ,li').bind('click', function(){
+
+        if(show_submenu1 == 0){
+            $(this).children("ul").slideDown();            
+            show_submenu1 = 1;
+        }else{
+            $(this).children("ul").slideUp();
+            show_submenu1 = 0;
+        }
+        
+    })        
+    $('.submenu-web-ul-li-ul ,li').bind('click', function(){
+
+        if(show_submenu2 == 0 ){
+            $(this).children("ul").slideDown();                 
+            show_submenu2 = 1;
+        }else{
+            $(this).children("ul").slideUp();
+            show_submenu2 = 0;                                  
+        }
+        
+    })
+    $('.menu ul ,li').on('click', function(e){
+        e.stopInmediatePropagation()
+    })
 }
-
-
 
 $(window).resize(function(){
     
