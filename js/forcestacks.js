0c0c6df11c78f980a17181764cc66f3cf03df0d1
castelll0009 <castelll0009@gmail.com>
2021-04-09 22:27:10 -0500

    controlando visivilidad del button-previus-pc TODO
    Melopelo que no puede

--- js/forcestacks.js
+++ js/forcestacks.js
@@ -1,17 +1,30 @@
+/* DECLARACION DE FUNCIONES Y VARIABLES GLOBALES*/
 var step1 = document.getElementById("step1");
 var step2 =  document.getElementById("step2");
 var step3 =  document.getElementById("step3");
 var step4 =  document.getElementById("step4");
 var step5 =  document.getElementById("step5");
 
+
 function gotoStep2(){
     step1.style ="display: none";
     step2.style ="display: block";
 }
+
+var buttonPreviusPC = document.getElementById("button-previus-pc");
+
+var stepcurrent = 1;
+function navigation(stepcurrent){        
+    if(stepcurrent == 1)
+        buttonPreviusPC.style = "display: none";    
+}
+/* IMPLEMENTACION O LLAMADO DE LAS FUNCIONES*/
+navigation();
+
 function handleClick(element){
 /*    element.style="display:none;"**/    
     if(element == 'youtube'){
-        gotoStep2();
+        gotoStep2();        
     }
     if(element == 'hbomax'){
         gotoStep2();
