f0c034f59781d32314311aae69f9af39b7812728
castelll0009 <castelll0009@gmail.com>
2021-04-09 20:45:24 -0500

    js pasar al segundo click paso
    haciendo click en alguna de las tarjetas.

--- js/forcestacks.js
+++ js/forcestacks.js
@@ -4,11 +4,34 @@ var step3 =  document.getElementById("step3");
 var step4 =  document.getElementById("step4");
 var step5 =  document.getElementById("step5");
 
-function handleClick(element){
-/*    element.style="display:none;"**/    
+function gotoStep2(){
     step1.style ="display: none";
     step2.style ="display: block";
-
+}
+function handleClick(element){
+/*    element.style="display:none;"**/    
+    if(element == 'youtube'){
+        gotoStep2();
+    }
+    if(element == 'hbomax'){
+        gotoStep2();
+    }
+    if(element == 'netflix'){
+        gotoStep2();
+    }
+    if(element == 'disney'){
+        gotoStep2();
+    }
+    if(element == 'spotify'){
+        gotoStep2();
+    }
+    if(element == 'crunchyroll'){
+        gotoStep2();
+    }
+    if(element == 'primevideo'){
+        gotoStep2();
+    }
+    
 }
 
 
